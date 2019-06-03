0025f6412d1a8085987e80465482cdc5dff4f701
Wilton Garcia <wiltongarciaweb@gmail.com>
2019-06-02 23:47:01 -0300

    Commit 04
    - Ajuste no script SQL
    - Ajuste no html de cadastro
    - Ajuste no php de cadastrp

--- BancoETabela.sql
+++ BancoETabela.sql
@@ -1,9 +1,9 @@
 create DATABASE receitasweb;
 use receitasweb;
 create table receitas(
-	 codigoReceita int(9) PRIMARY KEY AUTO_INCREMENT,
-     nome varchar(150) not null,
-     url varchar(500),
+	 codigo int(9) PRIMARY KEY AUTO_INCREMENT,
+     titulo varchar(150) not null,
+     urlimg varchar(500),
      receita text,
-     votor int(5) DEFAULT 0
+     votos int(5) DEFAULT 0
 );
--- cadastro.php
+++ cadastro.php
@@ -1,17 +1,25 @@
 <?php
+
     /*Faz conexão com o banco de dados*/
-    $connect = mysql_connect('nome_do_servidor','nome_de_usuario','senha');
+    $connect = mysqli_connect('localhost','root');
+    /*Escolhe o banco de dados a ser conectado*/
+    $db = mysqli_select_db($connect,'receitasweb');
     /*Váriaveies do formulário*/
     $titulo = $_POST['titulo'];
-    $url = $_POST['url']);
-    $receita = $_POST['receita']);
+    $urlimg = $_POST['urlimg'];
+    $receita = $_POST['receita'];
     /*Insere dados na tabela de receitas*/
-    $query = "INSERT INTO usuarios (titulo,url,senha) VALUES ('$titulo','$url','$receita')";
+    $query = "INSERT INTO receitas (titulo,urlimg,receita) VALUES ('$titulo','$urlimg','$receita')";
     /*Executa a query. É necessário passar a string de conexão com o banco*/
-    $insert = mysql_query($query,$connect);
+    $insert = mysqli_query($connect,$query) or die (mysqli_error($connect));;
     /*Se a consulta executou com sucesso*/
     if($insert){
         echo"<script language='javascript' type='text/javascript'>
                 alert('Usuário cadastrado com sucesso!');window.location.href='cadastro.html'
             </script>";
+    }else{
+        echo"<script language='javascript' type='text/javascript'>
+        alert('Não foi possível cadastrar esse usuário');window.location
+        .href='cadastro.html'</script>";
+    }
 ?>
